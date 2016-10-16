be70e78468419db7bf7406256aec63636136cc8d
illynabruin92 <alvaivette@g.ucla.edu>
2016-10-16 12:55:57 -0700

    delete orm file, update burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ router.get('/burgers', function(req,res) {
 	//express callback response by calling burger.selectAllBurger
 	burger.all(function(burger_data){
 		//wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-		res.render('index', {burger_data});
+		//res.render('index', {burger_data});
 	});
 });
 
@@ -20,8 +20,8 @@ router.post('/burgers/create', function(req, res) {
 	//takes the request object using it as input for buger.addBurger
 	burger.create(req.body.burger_name, function(result){
 		//wrapper for orm.js that using MySQL insert callback will return a log to console, render back to index with handle
-		console.log(result);
-		res.redirect('/');
+		//console.log(result);
+		//res.redirect('/');
 	});
 });
 
@@ -29,8 +29,8 @@ router.post('/burgers/create', function(req, res) {
 router.put('/burgers/update', function(req,res){
 	burger.update(req.body.burger_id, function(result){
 		//wrapper for orm.js that using MySQL update callback will return a log to console, render back to index with handle
-		console.log(result);
-		res.redirect('/');
+		//console.log(result);
+		//res.redirect('/');
 	});
 });
 
